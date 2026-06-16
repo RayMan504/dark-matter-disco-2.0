4b1a55ff7fae2a5758bfc1b06791f6e0ee48f41e
RayMan504 <uraeusmillet@gmail.com>
2026-06-15 20:06:04 -0500

    generate and render toolbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import randomNames from '../assets/random-usernames';
+import { Toolbar } from "./toolbar/toolbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
