85564e37c4289b83bbc7439fb5b304c4bc729ed5
RayMan504 <uraeusmillet@gmail.com>
2026-06-17 14:27:11 -0500

    star component + static user and star count

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,6 +12,8 @@ import { Toolbar } from "./toolbar/toolbar";
 export class App {
   protected readonly title = signal('dark-matter-disco-2.0');
   // TODO: setup default values for the app, such as username, stars, and other settings.
-  hostUsername: string = randomNames[Math.floor(Math.random() * randomNames.length)]; 
+  hostUsername: string = randomNames[Math.floor(Math.random() * randomNames.length)];
+  username: string | null = randomNames[Math.floor(Math.random() * randomNames.length)];
+  userStars: number = 0;
   inviteeUsername: string | null = null;
 }
---
+++ src/app/stars/stars.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stars',
+  imports: [],
+  templateUrl: './stars.html',
+  styleUrl: './stars.scss',
+})
+export class Stars {}
