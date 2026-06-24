4a72dda0a8cf5d61c82efb395d2f267ce8c3066e
RayMan504 <uraeusmillet@gmail.com>
2026-06-23 19:13:27 -0500

    camera service pseudocode

---
+++ src/app/services/camera.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Camera {
+  // setup camera
+  // steup stream
+
+
+  // method to initialize camera
+    // access video element
+    //  get latest media stream
+    // append media stream to video element
+    //  play video
+}
