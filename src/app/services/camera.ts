fd1bbd14fd8a0b17ce60b000737a6a12219253a8
RayMan504 <uraeusmillet@gmail.com>
2026-06-23 19:33:31 -0500

    camera service tests pass

--- src/app/services/camera.ts
+++ src/app/services/camera.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Camera {
-  // setup camera
-  // steup stream
-
-
-  // method to initialize camera
-    // access video element
+export class CameraService {
+  // method to initialize camera: takes a video element as parameter and returns a promise
+  async initCamera(video: HTMLVideoElement): Promise<void> {
     //  get latest media stream
-    // append media stream to video element
+    const stream  = await navigator.mediaDevices.getUserMedia({ video: true });
+    //  append media stream to video element
+    video.srcObject = stream;
     //  play video
+    await video.play();
+  }
 }
