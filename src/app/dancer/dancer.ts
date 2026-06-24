2083c1b1c5b20bcd54f93d2d1ddffe0fbc35460c
RayMan504 <uraeusmillet@gmail.com>
2026-06-23 21:51:06 -0500

    target video component only renders pixi graphics

--- src/app/dancer/dancer.ts
+++ src/app/dancer/dancer.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CameraService } from '../services/camera';
 import { PixiGraphicsService } from '../services/pixi-graphics/pixi-graphics';
+import { PoseDetectionService } from '../services/pose-detection/pose-detection';
 
 @Component({
   selector: 'app-dancer',
@@ -11,12 +12,16 @@ import { PixiGraphicsService } from '../services/pixi-graphics/pixi-graphics';
 export class Dancer implements AfterViewInit {
   @ViewChild('video') videoRef!: ElementRef<HTMLVideoElement>;
 
-  constructor(private cameraService: CameraService, private pixiGraphicsService: PixiGraphicsService) {}
+  constructor(private cameraService: CameraService, private pixiGraphicsService: PixiGraphicsService, private poseDetectionService: PoseDetectionService) {}
 
 
   async ngAfterViewInit(): Promise<void> {
       const videoElement = this.videoRef.nativeElement;
       await this.cameraService.initCamera(videoElement);
       await this.pixiGraphicsService.initPixi(videoElement);
+      await this.poseDetectionService.initializePoseLandmarker();
+      const poseLandmarker = this.poseDetectionService.getInstance();
+      console.log(poseLandmarker);
+      this.pixiGraphicsService.startDetection(videoElement, poseLandmarker);
   }
 }
--- src/app/services/pixi-graphics/pixi-graphics.ts
+++ src/app/services/pixi-graphics/pixi-graphics.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Application, Graphics, Sprite } from 'pixi.js';
+import { PoseLandmarker } from '@mediapipe/tasks-vision';
+
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +19,7 @@ export class PixiGraphicsService {
     await this.app.init({
       width: 640,
       height: 480,
+      backgroundAlpha: 0
     })
 
     // PIXI sprite and  graphics setup
@@ -24,10 +27,44 @@ export class PixiGraphicsService {
     this.graphics = new Graphics();
     this.sprite.width = this.app.screen.width;
     this.sprite.height = this.app.screen.height;
-    this.app.stage.addChild(this.sprite);
+    // this.app.stage.addChild(this.sprite);
     this.app.stage.addChild(this.graphics);
 
     // Append PIXI canvas to the DOM
     document.body.appendChild(this.app.canvas);
+  }
+
+  // attempt to track poses
+  public startDetection(video: HTMLVideoElement, poseLandmarker: PoseLandmarker): void {
+
+    this.app.ticker.add(() => {
+      // update the sprite texture with the current video frame
+      this.sprite.texture.update();
+
+      // get video frame and detect poses
+      const result =
+        poseLandmarker.detectForVideo(
+          video,
+          performance.now()
+        );
+
+      if (!result.landmarks?.length) {
+        return;
+      }
+
+      // clear graphics before rendering new landmarks
+      this.graphics.clear();
+
+      // bound sprites in order to render within canvas
+      const bounds = this.sprite.getBounds();
+      for (const lm of result.landmarks[0]) {
+        const x = bounds.x + lm.x * bounds.width;
+        const y = bounds.y + lm.y * bounds.height;
+
+        // render graphics for each landmark
+        this.graphics.fill(0xff0000);
+        this.graphics.circle(x, y, 4);
+      }
+    });
   }
 }
